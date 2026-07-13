2db4fdd73a3d7e9cf514eee048178bb015cd75fa
smartfeni <smartfeni247@gmail.com>
2026-07-13 08:20:14 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
-import llmsTxt from '@waldheimdev/astro-ai-llms-txt';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: false,
+    },
+    speedInsights: {
+      enabled: false,
+    },
+  }),
   site: 'https://smart-feni-murex.vercel.app',
   trailingSlash: 'ignore',
-  integrations: [
-    llmsTxt({
-      include: ['/**'],
-      exclude: ['/api/**'],
-    })
-  ]
 });
