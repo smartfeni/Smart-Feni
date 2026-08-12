0aeaf8b444369b54725236cd37b39d5b66e68d9f
smartfeni <smartfeni247@gmail.com>
2026-08-12 04:15:46 +0300

    Create public/sw.js

---
+++ public/public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "smartfeni-v1";
+const OFFLINE_URL = "/";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(OFFLINE_URL))
+    );
+  }
+});
