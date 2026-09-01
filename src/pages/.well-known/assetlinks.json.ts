3c86df041fe0628447b194de03493b0660f76c51
smartfeni <smartfeni247@gmail.com>
2026-09-01 22:05:29 +0300

    Update assetlinks.json.ts

--- src/pages/.well-known/assetlinks.json.ts
+++ src/pages/.well-known/assetlinks.json.ts
@@ -9,7 +9,8 @@ const assetlinks = [
       namespace: "android_app",
       package_name: "com.smartfeni.app",
       sha256_cert_fingerprints: [
-        "9B:80:0E:45:D0:B2:6A:0D:C5:54:76:4F:CD:E7:7D:83:2A:E1:D6:33:88:E9:F8:95:DB:54:4E:4F:01:5F:3E:46"
+        "9B:80:0E:45:D0:B2:6A:0D:C5:54:76:4F:CD:E7:7D:83:2A:E1:D6:33:88:E9:F8:95:DB:54:4E:4F:01:5F:3E:46",
+        "2C:2D:25:59:5A:FF:44:48:0F:56:CF:5E:CD:53:64:99:E0:77:FA:23:1C:3E:BD:76:16:E3:96:B9:6B:BA:56:CC"
       ]
     }
   }
